426fa20e53a75ef459fc91f33cbd7bcdd56522a3
Zac <1390919155@qq.com>
2023-12-05 17:05:26 +0800

    EP 7

---
+++ build/CMakeFiles/in1week.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/in1week.dir/src/main.cpp.obj"
+  "CMakeFiles/in1week.dir/src/main.cpp.obj.d"
+  "in1week.exe"
+  "in1week.exe.manifest"
+  "in1week.pdb"
+  "libin1week.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/in1week.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/in1week.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for in1week.
+# This may be replaced when dependencies are built.
